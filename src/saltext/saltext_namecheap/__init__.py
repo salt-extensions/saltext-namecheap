salt-extensions/saltext-namecheap

commit 2dddc9245c09428b2648ffd80f3d44606f6dc659

Improved logging configuration options from the configuration files.
* Added the ability to configure the log levels for the console logger separate from the logfile logger. The log level provided from the cli script will override the console log level set on the configuration file but not the logfile logger setting, unless theres no setting for the logfile setting in which case it will be the same as the console log level.
* Added the ability to provide the logging formats for both the console and the logfile.
* Added the ability to provide the date format to be used.
* The module name formatting("%(name)s") now remembers the widest name, this will improve log readability.
* Also catch `KeyboardInterrupt`s while the minion is waiting to connect to the master and not just on `minion.tune_in()`.

All these new configuration settings are documented on the configuration file templates.

src/saltext/saltext_namecheap/__init__.py
@@ -18,15 +18,12 @@ except ImportError as e:
     if e.args[0] != 'No module named _msgpack':
         raise
 
-log_format = '%(asctime)s,%(msecs)03.0f [%(name)-15s][%(levelname)-8s] %(message)s'
-
 class Master(object):
     '''
     Creates a master server
     '''
     def __init__(self):
         self.cli = self.__parse_cli()
-        self.opts = salt.config.master_config(self.cli['config'])
         # command line overrides config
         if self.cli['user']:
             self.opts['user'] = self.cli['user']
@@ -62,14 +59,23 @@ class Master(object):
         parser.add_option('-l',
                 '--log-level',
                 dest='log_level',
-                default='warning',
                 choices=list(salt.log.LOG_LEVELS),
                 help='Console log level. One of %s. For the logfile settings '
-                     'see the config file. Default: \'%%default\'.' %
+                     'see the config file. Default: \'warning\'.' %
                      ', '.join([repr(l) for l in salt.log.LOG_LEVELS]))
 
         options, args = parser.parse_args()
-        salt.log.setup_console_logger(options.log_level, log_format=log_format)
+
+        self.opts = salt.config.master_config(options.config)
+
+        if not options.log_level:
+            options.log_level = self.opts['log_level']
+
+        salt.log.setup_console_logger(
+            options.log_level,
+            log_format=self.opts['log_fmt_console'],
+            date_format=self.opts['log_datefmt']
+        )
 
         cli = {'daemon': options.daemon,
                'config': options.config,
@@ -92,12 +98,17 @@ class Master(object):
                     self.opts['sock_dir'],
                     ],
                     self.opts['user'])
+
         import salt.log
         salt.log.setup_logfile_logger(
-            self.opts['log_file'], self.opts['log_level']
+            self.opts['log_file'],
+            self.opts['log_level_logfile'] or self.opts['log_level'],
+            log_format=self.opts['log_fmt_logfile'],
+            date_format=self.opts['log_datefmt']
         )
         for name, level in self.opts['log_granular_levels'].items():
             salt.log.set_logger_level(name, level)
+
         import logging
         log = logging.getLogger(__name__)
         # Late import so logging works correctly
@@ -121,7 +132,6 @@ class Minion(object):
     '''
     def __init__(self):
         self.cli = self.__parse_cli()
-        self.opts = salt.config.minion_config(self.cli['config'])
         # command line overrides config
         if self.cli['user']:
             self.opts['user'] = self.cli['user']
@@ -155,14 +165,23 @@ class Minion(object):
         parser.add_option('-l',
                 '--log-level',
                 dest='log_level',
-                default='warning',
                 choices=list(salt.log.LOG_LEVELS),
                 help='Console log level. One of %s. For the logfile settings '
-                     'see the config file. Default: \'%%default\'.' %
+                     'see the config file. Default: \'warning\'.' %
                      ', '.join([repr(l) for l in salt.log.LOG_LEVELS]))
 
         options, args = parser.parse_args()
-        salt.log.setup_console_logger(options.log_level, log_format=log_format)
+
+        self.opts = salt.config.minion_config(options.config)
+
+        if not options.log_level:
+            options.log_level = self.opts['log_level']
+
+        salt.log.setup_console_logger(
+            options.log_level,
+            log_format=self.opts['log_fmt_console'],
+            date_format=self.opts['log_datefmt']
+        )
 
         cli = {'daemon': options.daemon,
                'config': options.config,
@@ -181,12 +200,17 @@ class Minion(object):
             os.path.dirname(self.opts['log_file']),
                 ],
                 self.opts['user'])
+
         import salt.log
         salt.log.setup_logfile_logger(
-            self.opts['log_file'], self.opts['log_level']
+            self.opts['log_file'],
+            self.opts['log_level_logfile'] or self.opts['log_level'],
+            log_format=self.opts['log_fmt_logfile'],
+            date_format=self.opts['log_datefmt']
         )
         for name, level in self.opts['log_granular_levels'].items():
             salt.log.set_logger_level(name, level)
+
         import logging
         # Late import so logging works correctly
         import salt.minion
@@ -195,18 +219,18 @@ class Minion(object):
             # Late import so logging works correctly
             import salt.utils
             # If the minion key has not been accepted, then Salt enters a loop
-            # waiting for it, if we daemonize later then the minion cound halt
+            # waiting for it, if we daemonize later then the minion could halt
             # the boot process waiting for a key to be accepted on the master.
             # This is the latest safe place to daemonize
             salt.utils.daemonize()
-        minion = salt.minion.Minion(self.opts)
-        set_pidfile(self.cli['pidfile'])
-        if check_user(self.opts['user'], log):
-            try:
+        try:
+            minion = salt.minion.Minion(self.opts)
+            set_pidfile(self.cli['pidfile'])
+            if check_user(self.opts['user'], log):
                 minion.tune_in()
-            except KeyboardInterrupt:
-                log.warn('Stopping the Salt Minion')
-                raise SystemExit('\nExiting on Ctrl-c')
+        except KeyboardInterrupt:
+            log.warn('Stopping the Salt Minion')
+            raise SystemExit('\nExiting on Ctrl-c')
 
 
 class Syndic(object):
@@ -215,7 +239,6 @@ class Syndic(object):
     '''
     def __init__(self):
         self.cli = self.__parse_cli()
-        self.opts = self.__prep_opts()
         # command line overrides config
         if self.cli['user']:
             self.opts['user'] = self.cli['user']
@@ -275,14 +298,22 @@ class Syndic(object):
         parser.add_option('-l',
                 '--log-level',
                 dest='log_level',
-                default='warning',
                 choices=list(salt.log.LOG_LEVELS),
                 help='Console log level. One of %s. For the logfile settings '
-                     'see the config file. Default: \'%%default\'.' %
+                     'see the config file. Default: \'warning\'.' %
                      ', '.join([repr(l) for l in salt.log.LOG_LEVELS]))
 
         options, args = parser.parse_args()
-        salt.log.setup_console_logger(options.log_level, log_format=log_format)
+        self.opts = self.__prep_opts()
+
+        if not options.log_level:
+            options.log_level = self.opts['log_level']
+
+        salt.log.setup_console_logger(
+            options.log_level,
+            log_format=self.opts['log_fmt_console'],
+            date_format=self.opts['log_datefmt']
+        )
 
         cli = {'daemon': options.daemon,
                'minion_config': options.minion_config,

src/saltext/saltext_namecheap/utils/__init__.py
@@ -312,11 +312,16 @@ def dns_check(addr, safe=False):
         try:
             addr = socket.gethostbyname(addr)
         except socket.gaierror:
-            err = ('This master address: {0} was previously resolvable but '
+            err = ('This master address: \'{0}\' was previously resolvable but '
                   'now fails to resolve! The previously resolved ip addr '
                   'will continue to be used').format(addr)
             if safe:
-                log.error(err)
+                import salt.log
+                if salt.log.is_console_configured():
+                    # If logging is not configured it also means that either
+                    # the master or minion instance calling this hasn't even
+                    # started running
+                    log.error(err)
                 raise SaltClientError
             else:
                 err = err.format(addr)

commit 31eba51634c84b82899777afd45d265809a592ce

Fixes syndic init failure due to cli attribute error introduced in commit: 2dddc9245c09428b2648ffd80f3d44606f6dc659

src/saltext/saltext_namecheap/__init__.py
@@ -258,13 +258,13 @@ class Syndic(object):
         if self.cli['user']:
             self.opts['user'] = self.cli['user']
 
-    def __prep_opts(self):
+    def __prep_opts(self, cli):
         '''
         Generate the opts used by the syndic
         '''
-        opts = salt.config.master_config(self.cli['master_config'])
+        opts = salt.config.master_config(cli['master_config'])
         opts['_minion_conf_file'] = opts['conf_file']
-        opts.update(salt.config.minion_config(self.cli['minion_config']))
+        opts.update(salt.config.minion_config(cli['minion_config']))
         if 'syndic_master' in opts:
             # Some of the opts need to be changed to match the needed opts
             # in the minion class.
@@ -319,7 +319,14 @@ class Syndic(object):
                      ', '.join([repr(l) for l in salt.log.LOG_LEVELS]))
 
         options, args = parser.parse_args()
-        self.opts = self.__prep_opts()
+
+        cli = {'daemon': options.daemon,
+               'minion_config': options.minion_config,
+               'master_config': options.master_config,
+               'pidfile': options.pidfile,
+               'user': options.user}
+
+        self.opts = self.__prep_opts(cli)
 
         if not options.log_level:
             options.log_level = self.opts['log_level']
@@ -329,12 +336,6 @@ class Syndic(object):
             log_format=self.opts['log_fmt_console'],
             date_format=self.opts['log_datefmt']
         )
-
-        cli = {'daemon': options.daemon,
-               'minion_config': options.minion_config,
-               'master_config': options.master_config,
-               'pidfile': options.pidfile,
-               'user': options.user}
 
         return cli
 